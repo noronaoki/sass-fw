b7f3c62658d3a1236b9e909c50024c8856d08748
noro <naoki_noro@voyagegroup.com>
2014-11-26 19:41:08 +0900

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,85 @@
+var gulp        = require('gulp'),
+    sass        = require('gulp-ruby-sass'),
+    plumber     = require('gulp-plumber'),
+    spritesmith = require('gulp.spritesmith'),
+    pleeease    = require('gulp-pleeease'),
+    tinypng     = require('gulp-tinypng'),
+    styledocco  = require('gulp-styledocco'),
+    browserSync = require('browser-sync');
+
+// sass,pleeease
+gulp.task('sass', function() {
+  gulp.src('sass/**/*.scss')
+    .pipe(plumber())
+    .pipe(sass({
+      style: 'expanded',
+      sourcemap:false,
+      sourcemapPath:'assets/css/style.css.map'
+      /*# sourceMappingURL=style.css.map */
+    }))
+    .pipe(pleeease({
+        autoprefixer: {
+            browsers: ['last 30 version','Firefox >= 20','ie 8','ie 9']
+        },
+        rem:false,
+        minifier: false // minify無効
+    }))
+    .pipe(gulp.dest('assets/css/'));
+});
+
+// sprite
+gulp.task('sprite', function () {
+  var spriteData = gulp.src('tiny/sprite/*.png')
+  .pipe(spritesmith({
+    imgName: 'sprite.png', //スプライトの画像
+    cssName: '_sprite.scss', //生成されるscss
+    imgPath: '../img/sprite.png', //生成されるscssに記載されるパス
+    cssFormat: 'scss', //フォーマット
+    cssVarMap: function (sprite) {
+      sprite.name = 'sprite-' + sprite.name; //VarMap(生成されるScssにいろいろな変数の一覧を生成)
+    }
+  }));
+  spriteData.img.pipe(gulp.dest('assets/img/')); //imgNameで指定したスプライト画像の保存先
+  spriteData.css.pipe(gulp.dest('sass/')); //cssNameで指定したcssの保存先
+});
+
+
+// tiny png
+gulp.task('tinypng', function () {
+    gulp.src(['tiny/*.png', 'tiny/*.jpg'])
+        .pipe(tinypng('pDRurCCYmFqEjjA6qfXk85_CbkKG-POI')) // API KEYは自分の使ってください
+        .pipe(gulp.dest('assets/img/'));
+});
+
+
+
+// styledocco
+gulp.task('styledocco', function () {
+  gulp.src('assets/css/style.css')
+    .pipe(styledocco({
+      out: 'styleguide',
+      name: 'sass-for-designers'
+    }));
+});
+
+
+// browserSync
+gulp.task('browser-sync', function(){
+  browserSync({
+    server: {
+      baseDir: './'
+    }
+  });
+});
+
+// bs-reload
+gulp.task('bs-reload', function(){
+  browserSync.reload();
+});
+
+
+gulp.task('default', ['sass', 'sprite', 'tinypng', 'styledocco', 'browser-sync'],function(){
+  gulp.watch('sass/**/*.scss', ['sass']);
+  gulp.watch('assets/css/**/*.css', ['styledocco']);
+  gulp.watch(['./*.html', 'assets/css/**/*.css'], ['bs-reload']);
+});
---
+++ modules.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+npm install --save-dev gulp
+npm install --save-dev gulp-pleeease
+npm install --save-dev gulp-ruby-sass
+npm install --save-dev gulp-plumber
+npm install --save-dev gulp-tinypng
+npm install --save-dev gulp-styledocco
+npm install --save-dev gulp.spritesmith
+npm install --save-dev browser-sync
