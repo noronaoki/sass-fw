b3586a363499090826080cd3b2033241d9e722b3
野呂 直樹 <naoki_noro@voyagegroup.com>
2015-05-13 17:59:23 +0900

    installパッケージリストが足りなかったので追加

--- modules.sh
+++ modules.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+npm install -g gulp
+npm install -g styledocco
 npm install --save-dev gulp
 npm install --save-dev gulp-pleeease
 npm install --save-dev gulp-ruby-sass
@@ -8,6 +10,6 @@ npm install --save-dev gulp-tinypng
 npm install --save-dev gulp-styledocco
 npm install --save-dev gulp.spritesmith
 npm install --save-dev browser-sync
-npm install -g gulp
-npm install gulp
-npm install -g styledocco
+npm install --save-dev gulp-sourcemaps
+npm install --save-dev gulp-if
+npm install --save-dev gulp-footer
