902ebcc86156a45f264d1fdbc32b86ac2f2fec75
自宅 <noro0617@gmail.com>
2016-06-19 14:06:12 +0900

    npm init追加、brower-sync戻した

--- modules.sh
+++ modules.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
+npm init
 npm install gulp -g
-npm install devbridge-styleguide -g
+# npm install devbridge-styleguide -g
 npm install live-server -g
 npm install gulp --save-dev
 npm install gulp-pleeease --save-dev
@@ -9,8 +10,8 @@ npm install gulp-ruby-sass --save-dev
 npm install gulp-plumber --save-dev
 npm install gulp-tinypng --save-dev
 npm install gulp.spritesmith --save-dev
-# npm install --save-dev browser-sync
+npm install --save-dev browser-sync
 npm install gulp-sourcemaps --save-dev
 # npm install --save-dev gulp-if
 # npm install --save-dev gulp-footer
-npm install devbridge-styleguide --save-dev
+# npm install devbridge-styleguide --save-dev
