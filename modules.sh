3e3a554766faa34e9a2c6bbaebba2f9dc3e48f44
noronaoki <naoki_noro@voyagegroup.com>
2016-09-09 18:24:28 +0900

    整えた

--- modules.sh
+++ modules.sh
@@ -2,16 +2,15 @@
 
 npm init
 npm install gulp -g
-# npm install devbridge-styleguide -g
-npm install live-server -g
 npm install gulp --save-dev
 npm install gulp-pleeease --save-dev
 npm install gulp-ruby-sass --save-dev
 npm install gulp-plumber --save-dev
 npm install gulp-tinypng-compress --save-dev
 npm install gulp.spritesmith --save-dev
-npm install --save-dev browser-sync
+npm install browser-sync --save-dev
 npm install gulp-sourcemaps --save-dev
 # npm install --save-dev gulp-if
 # npm install --save-dev gulp-footer
-# npm install devbridge-styleguide --save-dev
+npm install devbridge-styleguide -g
+npm install devbridge-styleguide --save-dev
